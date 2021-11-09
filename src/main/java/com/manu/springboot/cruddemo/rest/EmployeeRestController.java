799f66a12ece59798e81e5ae2975c6e139265ad3
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-11-09 19:16:27 +0100

    Create a service to delegate the calls to the DAO

--- src/main/java/com/manu/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/manu/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.manu.springboot.cruddemo.dao.EmployeeDAO;
 import com.manu.springboot.cruddemo.entity.Employee;
+import com.manu.springboot.cruddemo.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
-
+    private EmployeeService employeeService;
     // quick and dirty: inject employee dao
     @Autowired
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
     }
 
     // expose "/employees" and return list of employees
     @GetMapping("/employees")
     public List<Employee> findall() {
 
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
---
+++ src/main/java/com/manu/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.manu.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.manu.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
+}
