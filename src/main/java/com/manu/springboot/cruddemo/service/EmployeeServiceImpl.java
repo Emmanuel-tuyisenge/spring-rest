c4902def8bec3447f3e0935152084c101f9ccd96
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-11-10 15:10:27 +0100

    crud using JPA and DAO implementation

--- src/main/java/com/manu/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/manu/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.manu.springboot.cruddemo.dao.EmployeeDAO;
 import com.manu.springboot.cruddemo.entity.Employee;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeDAO employeeDAO;
 
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDao) {
+    // add @Qualifier to specify wich DAO to use, so the BeanId is employeeDAOJpaImpl
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl")EmployeeDAO theEmployeeDao) {
         employeeDAO = theEmployeeDao;
     }
 
